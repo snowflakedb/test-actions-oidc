b918e1e0345e11c6432bc4bec95ce530d2ca0cfd
Ansh Jaiswal <ansh.jaiswal@snowflake.com>
2024-09-10 20:40:41 +0530

    printing secrets

--- github-api.py
+++ github-api.py
@@ -8,6 +8,7 @@ api_secret = os.getenv('API_SECRET')
 
 # Use the secret in your logic (e.g., making an API call)
 # Make sure not to log or print the secret
-print(api_secret)
+print(api_secret[0])
+print(api_secret[1])
 
 print("API secret fetched and being used securely.")
